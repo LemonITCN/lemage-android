ab18bebd0fb6c6ea71f7ce78319494b2dea3fe06
LemonITCN <liuri@lemonit.cn>
2018-06-21 16:40:33 +0800

    完成Lemage中的函数定义

---
+++ lemage/lemage-lib/src/main/java/cn/lemonit/lemage/Lemage.java
@@ -0,0 +1,156 @@
+package cn.lemonit.lemage;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.Serializable;
+import java.util.List;
+
+import cn.lemonit.lemage.bean.ImageSize;
+import cn.lemonit.lemage.interfaces.LemageResultCallback;
+
+/**
+ * Lemage入口类
+ *
+ * @author LemonIT.CN - liuri
+ */
+public class Lemage implements Serializable {
+
+    /**
+     * 启动Lemage
+     */
+    public static void startUp() {
+        BitmapDrawable drawable;
+    }
+
+
+    /**
+     * 根据Bitmap对象来生成LemageURL字符串
+     * 原理：将Bitmap转成二进制数据存储到沙盒中的文件，然后生成指向沙盒中二进制文件的Lemage格式的URL
+     * <p>
+     * lemage://album/local/xxxxxxxxxxxxxxxxxxxxx
+     * lemage://sandbox/long[short]/xxxxxxxxxxxxxxxx
+     *
+     * @param bitmap   要生成LemageURL的Bitmap对象
+     * @param longTerm 是否永久有效，如果传true，那么该URL直到调用Lemage.expiredAllLongTermUrl方法后才失效，如果传false，在下次APP启动调用Lemage.startUp方法时URL就会失效，也可以通过Lemage.expiredAllShortTermUrl来强制使其失效
+     * @return 生成的LemageURL
+     */
+    public static String generateLemageUrl(Bitmap bitmap, boolean longTerm) {
+        return null;
+    }
+
+    /**
+     * 根据LemageURL加载对应的图片的输入流，如果用户传入的LemageURL有误或已过期，会返回null
+     * 注意：此方法并不会处理图片的缩放参数，即LemageURL中的width参数和height参数会被忽略，若需要请调用Lemage.loadImage方法
+     * 原理：根据LemageURL解析出沙盒对应的文件路径，然后从沙盒读取文件数据转换成FileInputStream对象后返回
+     *
+     * @param url LemageURL字符串
+     * @return 根据LemageURL逆向转换回来的图片FileInputStream对象，如果URL无效会返回null
+     */
+    public static FileInputStream loadImageInputStream(String url) {
+        return null;
+    }
+
+    /**
+     * 根据LemageURL加载对应的图片的输入流，如果用户传入的LemageURL有误或已过期，会返回null
+     * 注意：此方法并不会处理图片的缩放参数，即LemageURL中的width参数和height参数会被忽略，若需要请调用Lemage.loadImage方法
+     * 原理：根据LemageURL解析出沙盒对应的文件路径，然后从沙盒读取文件数据转换成byte[]后返回
+     *
+     * @param url LemageURL字符串
+     * @return 根据LemageURL逆向转换回来的图片byte[]，如果URL无效会返回null
+     */
+    public static byte[] loadImageData(String url) {
+        return null;
+    }
+
+    /**
+     * 根据LemageURL加载对应的图片的Bitmap对象，如果用户传入的LemageURL有误或已过期，会返回nil
+     * 该函数会解析size参数中的width、height参数，如果没有传这个参数，或参数无效，那么会返回原图
+     * 原理：根据LemageURL解析出沙盒对应的文件路径，然后从沙盒读取文件数据转换成FileInputStream后转换成Bitmap对象返回
+     *
+     * @param url  LemageURL字符串
+     * @param size 图片指定大小
+     * @return 根据LemageURL逆向转换回来的图片Bitmap对象，如果URL无效会返回null
+     */
+    public static Bitmap loadImage(String url, ImageSize size) {
+        return null;
+    }
+
+    /**
+     * 根据LemageURL加载对应的图片的Bitmap对象，如果用户传入的LemageURL有误或已过期，会返回nil
+     * 该函数会解析LemageURL中的width、height参数，如果LemageURL中不存在这两个参数，那么会返回原图
+     * 原理：从LemageURL中解析出width/height参数，并调用loadImage(String url, ImageSize size)方法
+     *
+     * @param url LemageURL字符串
+     * @return 根据LemageURL逆向转换回来的图片Bitmap对象，如果URL无效会返回null
+     */
+    public static Bitmap loadImage(String url) {
+        return null;
+    }
+
+    /**
+     * 让所有长期的LemageURL失效
+     * 原理：删除所有本地长期LemageURL对应的沙盒图片文件
+     */
+    public static void expiredAllLongTermUrl() {
+
+    }
+
+    /**
+     * 让所有短期的LemageURL失效
+     * 原理：删除所有本地短期LemageURL对应的沙盒图片文件
+     */
+    public static void expiredAllShortTermUrl() {
+
+    }
+
+    /**
+     * 强制让指定的LemageURL过期，不区分当前URL是长期还是短期
+     * 原理：删除这个LemageURL对应的沙盒图片文件
+     *
+     * @param url 要使其过期的LemageURL
+     */
+    public static void expiredUrl(String url) {
+
+    }
+
+    /**
+     * 启动图片选择器
+     *
+     * @param maxChooseCount         允许最多选择的图片张数，支持范围：1-99
+     * @param needShowOriginalButton 是否提供【原图】选项按钮，如果不提供，那么选择结果中的【用户是否选择了原图选项】会始终返回true
+     * @param themeColor             主题颜色，这个颜色会作为完成按钮、选择顺序标识、相册选择标识的背景色
+     * @param callback               结果回调函数，若用户在选择器中点击了取消按钮，那么回调函数中的imageUrlList为null
+     */
+    public static void startChooser(Integer maxChooseCount,
+                                    boolean needShowOriginalButton,
+                                    int themeColor,
+                                    LemageResultCallback callback) {
+
+    }
+
+    /**
+     * 启动图片预览器
+     *
+     * @param imageUrlArr       要预览的图片URL数组，支持lemageURL和http(s)URL如果对象为nil或数组为空，那么拒绝显示图片预览器
+     * @param chooseImageUrlArr 已经选择的图片Url数组
+     * @param allowChooseCount  允许选择的图片数量，如果传<=0的数，表示关闭选择功能（选择器右上角是否有选择按钮），如果允许选择数量大于chooseImageUrlArr数组元素数量，那么会截取choosedImageUrlArr中的数组前allowChooseCount个元素作为已选择图片
+     * @param showIndex         进入图片预览器后默认首先展示的图片索引
+     * @param themeColor        主题颜色，这个颜色会作为完成按钮、选择顺序标识的背景色
+     * @param callback          结果回调函数，若用户在选择器中点击了取消按钮，那么回调函数中的imageUrlList为null
+     */
+    public static void startPreviewer(List<String> imageUrlArr,
+                                      List<String> chooseImageUrlArr,
+                                      int allowChooseCount,
+                                      int showIndex,
+                                      int themeColor,
+                                      LemageResultCallback callback) {
+
+    }
+
+}
--- lemage/lemage-lib/src/main/java/cn/lemonit/lemage/interfaces/LemageResultCallback.java
+++ lemage/lemage-lib/src/main/java/cn/lemonit/lemage/interfaces/LemageResultCallback.java
@@ -2,26 +2,28 @@ package cn.lemonit.lemage.interfaces;
 
 import java.util.List;
 
-import cn.lemonit.lemage.bean.Photo;
-
 /**
- * Lemage图片回调
+ * Lemage通用图片结果毁掉函数
+ *
+ * @author LemonITCN - liuri
  */
 public interface LemageResultCallback {
 
 
     /**
-     * 页面即将关闭
-     * @param imageUrlList 图片的URL路径
-     * @param isOriginal 用户是否选中了原图选项
+     * 当界面即将被关闭的时候的回调函数
+     *
+     * @param imageUrlList 选择的图片对应的图片Url列表
+     * @param isOriginal   用户是否选择了原图选项，如果该组件关闭或不支持原图按钮选项，那么此值会始终返回true
      */
-    void willClose(List<String> imageUrlList,boolean isOriginal);
+    void willClose(List<String> imageUrlList, boolean isOriginal);
 
 
     /**
-     * 页面已经关闭
-     * @param imageUrlList 图片的URL列表
-     * @param isOriginal 用户是否选中了原图选项
+     * 当界面已经全部关闭的时候的回调函数
+     *
+     * @param imageUrlList 选择的图片对应的图片Url列表
+     * @param isOriginal   用户是否选择了原图选项，如果该组件关闭或不支持原图按钮选项，那么此值会始终返回true
      */
-    void closed(List<String> imageUrlList,boolean isOriginal);
+    void closed(List<String> imageUrlList, boolean isOriginal);
 }
